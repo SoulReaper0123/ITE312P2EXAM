0b2cd737b584cb3b7eb657083cb2ca1d4bfdfa2a
SoulReaper0123 <louisandredysarino@gmail.com>
2024-09-10 10:15:35 +0800

    last

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 import express from 'express';
-import exam from '../src/routes/exam.js';
-import subject from '../src/routes/subject.js';
-import mydata from '../src/routes/mydata.js';
-import node from '../src/routes/node.js';
+import exam from './routes/exam.js';
+import subject from './routes/subject.js';
+import mydata from './routes/mydata.js';
+import node from './routes/node.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.status(200).send('Welcome!');
+  res.status(200).send(`
+    Welcome!<br>
+    Available routes:<br>
+    /exam<br>
+    /mydata<br>
+    /node<br>
+    /subject
+  `);
 });
 
-app.use(exam);
-app.use(subject);
-app.use(mydata);
-app.use(node);
+app.use('/exam', exam);
+app.use('/subject', subject);
+app.use('/mydata', mydata);
+app.use('/node', node);
 
-
-// const PORT = 3333;
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
